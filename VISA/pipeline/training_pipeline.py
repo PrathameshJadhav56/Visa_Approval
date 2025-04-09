c2a679a8f98d949e6308b9b026a6e448bd177c29
Prathamesh Jadhav <prathamjadhav56@gmail.com>
2025-04-10 00:11:57 +0530

    data validation added

--- VISA/pipeline/training_pipeline.py
+++ VISA/pipeline/training_pipeline.py
@@ -2,19 +2,24 @@ import sys
 from VISA.exception import VisaException
 from VISA.logger import logging
 from VISA.components.data_ingestion import DataIngestion
+from VISA.components.data_validation import DataValidation
 
 
-from VISA.entity.config_entity import DataIngestionConfig
+from VISA.entity.config_entity import (DataIngestionConfig, 
+                                       DataValidationConfig)
 
 
 
-from VISA.entity.artifact_entity import DataIngestionArtifact
+
+from VISA.entity.artifact_entity import (DataIngestionArtifact,
+                                          DataValidationArtifact)
 
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
         
     
@@ -35,6 +40,32 @@ class TrainPipeline:
         except Exception as e:
             raise VisaException(e, sys) from e
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise VisaException(e, sys) from e
+        
+    
+        
         
     def run_pipeline(self, ) -> None:
         """
@@ -42,6 +73,6 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-        
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise VisaException(e, sys)
