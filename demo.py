b1a53a4f001332988b27730756df34e76d55fcf2
Prathamesh Jadhav <prathamjadhav56@gmail.com>
2025-04-09 22:46:57 +0530

    files updated

---
+++ demo.py
@@ -0,0 +1,4 @@
+from VISA.logger import logging
+from VISA.exception import VisaException
+
+logging.info("WELCOME")
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Visa2",
+    version="0.0.0",
+    author="Prathamesh",
+    author_email="prathamjadhav56@gmail.com",
+    packages=find_packages()
+)
